7316f4d5f0927842bcdeb70a31d1fd43d87d50b4
shamseena-dev <shamseenakarumarot@gmail.com>
2020-01-18 20:51:11 +0100

    added node modules and build for deploying

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -5,7 +5,7 @@ const Card=(props)=>{
 	return(
 		<div className="card">
 			<img src={props.image}	alt="news_image" />
-			<h1> {props.title}</h1>
+			<h3> {props.title}</h3>
 			<h5>Source : {props.source}</h5>
 			<p>{props.content}</p>
 			<h5><a href={props.url} target="_blank" rel="noopener noreferrer">Click here</a> to  Read more</h5>			
