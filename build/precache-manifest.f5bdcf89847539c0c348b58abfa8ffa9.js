f5d97f9ad45a2aac6e35a736d63122512aa33e52
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-05-31 20:40:26 +0200

    added files

---
+++ build/precache-manifest.f5bdcf89847539c0c348b58abfa8ffa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb692f73abba7b4e079390973e633ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf620db13cb3648c038",
+    "url": "/static/css/main.7520a22b.chunk.css"
+  },
+  {
+    "revision": "85f0dbe06926d83b23be",
+    "url": "/static/js/2.db4f2567.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.db4f2567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf620db13cb3648c038",
+    "url": "/static/js/main.0621e8a8.chunk.js"
+  },
+  {
+    "revision": "c48be26b3d9edd41ccc5",
+    "url": "/static/js/runtime-main.714d63fe.js"
+  },
+  {
+    "revision": "11ec11985387f99ba176eb60ee9452b0",
+    "url": "/static/media/news.11ec1198.jpg"
+  }
+]);
